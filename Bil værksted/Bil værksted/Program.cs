2f129290dc391ad6e06dd7a84f6b6c8bac6c53f6
Jeppe <jepnyvang31@gmail.com>
2019-11-04 10:06:38 +0100

    Car classes

---
+++ Bil værksted/Bil værksted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bil_værksted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
