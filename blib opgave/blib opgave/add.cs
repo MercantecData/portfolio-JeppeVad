38b097f90784a1e2583cac71999424693aed05f9
Jeppe <jepnyvang31@gmail.com>
2019-11-05 13:14:21 +0100

    This is the final product

---
+++ blib opgave/blib opgave/add.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blib_opgave
+{
+    class ADD
+    {
+        public string name;
+        public int age; 
+        public ADD(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public ADD(int age)
+        {
+            Console.WriteLine(age);
+        }
+
+    }
+}
