fa6b8c7bf68810f3651e8a90b2a9404593656bbf
Jeppe <jepnyvang31@gmail.com>
2019-11-06 08:36:07 +0100

    BicycleShop

---
+++ Cykel shop/Cykel shop/Bicycle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cykel_shop
+{
+    class Bicycle
+    {
+    }
+}
---
+++ Cykel shop/Cykel shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cykel_shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
