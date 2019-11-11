d884adaeb920ff242dac331e92e9bce763b51aef
Jeppe <jepnyvang31@gmail.com>
2019-11-11 14:19:21 +0100

    Added extra site to webpage

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,3 +1,6 @@
-function buttonClick() {
+function buttonClickHide() {
     document.getElementById('demo').style.display='none'
+}
+function buttonClickShow() {
+    document.getElementById('demo').style.display='block'
 }
