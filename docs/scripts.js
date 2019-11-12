7ebe08d5fca81d8939a08dbbdd2059ebf336dc48
Jeppe <jepnyvang31@gmail.com>
2019-11-12 14:40:36 +0100

    Todays progress

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,6 +1,17 @@
-function buttonClickHide() {
+function buttonClickHide() { //Denne fjerner billede
     document.getElementById('demo').style.display='none'
 }
-function buttonClickShow() {
+function buttonClickShow() { //Denne viser billede
     document.getElementById('demo').style.display='block'
+}
+function add() {
+    var p = document.createElement("p");
+    var text = document.createTextNode("Hey buddy!");
+    p.appendChild(text);
+    var element = document.getElementById('en');
+    element.appendChild(p);
+}
+function remove() {
+    var remove = document.getElementById('to');
+    remove.remove();
 }
